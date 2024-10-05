dd4b35044423be4e0bd30ce432d65c04b8a95a59
Artyom Volkov <tricky.fat.cat@gmail.com>
2024-10-05 23:29:51 +0300

    Create anim notify to set life span

---
+++ Source/LudumDare56/Animations/AnimNotify_SetLifeSpan.cpp
@@ -0,0 +1,12 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AnimNotify_SetLifeSpan.h"
+
+void UAnimNotify_SetLifeSpan::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation,
+	const FAnimNotifyEventReference& EventReference)
+{
+	Super::Notify(MeshComp, Animation, EventReference);
+
+	MeshComp->GetOwner()->SetLifeSpan(LifeSpan);
+}
---
+++ Source/LudumDare56/Animations/AnimNotify_SetLifeSpan.h
@@ -0,0 +1,24 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotify.h"
+#include "AnimNotify_SetLifeSpan.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LUDUMDARE56_API UAnimNotify_SetLifeSpan : public UAnimNotify
+{
+	GENERATED_BODY()
+
+public:
+	virtual void Notify(USkeletalMeshComponent* MeshComp,
+	                    UAnimSequenceBase* Animation,
+	                    const FAnimNotifyEventReference& EventReference) override;
+
+	UPROPERTY(EditAnywhere)
+	float LifeSpan = 1.0f;
+};
