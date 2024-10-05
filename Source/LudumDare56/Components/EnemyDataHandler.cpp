0e72e58149d0d544c65b6ac8ce0685a8bf78db98
Artyom Volkov <tricky.fat.cat@gmail.com>
2024-10-05 20:21:35 +0300

    Create enemy data handler component

---
+++ Source/LudumDare56/Components/EnemyDataHandler.cpp
@@ -0,0 +1,72 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EnemyDataHandler.h"
+
+#include "EnemyAttackComponent.h"
+#include "EnemyMovementComponent.h"
+#include "HitPointsComponent.h"
+#include "LudumDare56/Enemies/EnemyPawn.h"
+
+
+UEnemyDataHandler::UEnemyDataHandler()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+void UEnemyDataHandler::InitializeComponent()
+{
+	Super::InitializeComponent();
+
+	if (!GetWorld()->IsGameWorld())
+	{
+		return;
+	}
+
+	if (!EnemyDataTable || !GetOwner()->IsA(AEnemyPawn::StaticClass()))
+	{
+		return;
+	}
+
+	TArray<FEnemyData*> EnemyData;
+	EnemyDataTable->GetAllRows<FEnemyData>("", EnemyData);
+
+	if (EnemyData.IsEmpty())
+	{
+		return;
+	}
+
+	auto Predicate = [&](const FEnemyData* TargetData)
+	{
+		return TargetData->EnemyClass == GetOwner()->GetClass();
+	};
+
+	FEnemyData* Data = *EnemyData.FindByPredicate(Predicate);
+
+	if (!Data)
+	{
+		return;
+	}
+
+	UHitPointsComponent* HitPointsComponent = GetOwner()->GetComponentByClass<UHitPointsComponent>();
+
+	if (IsValid(HitPointsComponent))
+	{
+		HitPointsComponent->SetMaxHitPoints(Data->HitPoints);
+	}
+
+	UEnemyMovementComponent* EnemyMovementComponent = GetOwner()->GetComponentByClass<UEnemyMovementComponent>();
+
+	if (IsValid(EnemyMovementComponent))
+	{
+		EnemyMovementComponent->SetSpeed(Data->Speed);
+	}
+
+	UEnemyAttackComponent* AttackComponent = GetOwner()->GetComponentByClass<UEnemyAttackComponent>();
+	AttackComponent->Range = Data->AttackRange;
+	AttackComponent->Damage = Data->Damage;
+	AttackComponent->ProjectilesPerAttack = Data->ProjectilesPerAttack;
+	AttackComponent->AttackAngle = Data->AttackAngle;
+	AttackComponent->AttackMontage = Data->AttackAnimMontage;
+	AttackComponent->ProjectileClass = Data->ProjectileClass;
+}
---
+++ Source/LudumDare56/Components/EnemyDataHandler.h
@@ -0,0 +1,62 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "EnemyDataHandler.generated.h"
+
+class AEnemyPawn;
+class AEnemyProjectile;
+
+USTRUCT(BlueprintType)
+struct FEnemyData : public FTableRowBase
+{
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TSubclassOf<AEnemyPawn> EnemyClass = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (ClampMin = "1", UIMin = "1"))
+	int32 Experience = 10;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (ClampMin = "1", UIMin = "1"))
+	int32 HitPoints = 1;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (ClampMin = "1", UIMin = "1"))
+	int32 Speed = 600;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (ClampMin = "1", UIMin = "1"))
+	float AttackRange = 200.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (ClampMin = "1", UIMin = "1"))
+	int32 Damage = 1;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (ClampMin = "1", UIMin = "1"))
+	int32 ProjectilesPerAttack = 1;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (ClampMin = "1", UIMin = "1"))
+	float AttackAngle = 0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UAnimMontage* AttackAnimMontage = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TSubclassOf<AEnemyProjectile> ProjectileClass = nullptr;
+};
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class LUDUMDARE56_API UEnemyDataHandler : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UEnemyDataHandler();
+
+protected:
+	virtual void InitializeComponent() override;
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	UDataTable* EnemyDataTable = nullptr;
+};
