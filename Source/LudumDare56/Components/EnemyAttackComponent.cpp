4eab213a964bcb6fb4302ee69741bb8d7027dd3f
Artyom Volkov <tricky.fat.cat@gmail.com>
2024-10-05 18:52:19 +0300

    Create enemy attack component

---
+++ Source/LudumDare56/Components/EnemyAttackComponent.cpp
@@ -0,0 +1,79 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EnemyAttackComponent.h"
+
+#include "LudumDare56/Enemies/EnemyProjectile.h"
+
+
+UEnemyAttackComponent::UEnemyAttackComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+	bWantsInitializeComponent = true;
+}
+
+void UEnemyAttackComponent::InitializeComponent()
+{
+	Super::InitializeComponent();
+
+	if (GetWorld()->IsGameWorld())
+	{
+		USkeletalMeshComponent* SkeletalMeshComponent = GetOwner()->GetComponentByClass<USkeletalMeshComponent>();
+
+		if (IsValid(SkeletalMeshComponent))
+		{
+			AnimInstance = SkeletalMeshComponent->GetAnimInstance();
+		}
+	}
+}
+
+bool UEnemyAttackComponent::StartAttack()
+{
+	if (!IsValid(AnimInstance) && !IsValid(AttackMontage))
+	{
+		return false;
+	}
+
+	AnimInstance->Montage_Play(AttackMontage);
+	return true;
+}
+
+bool UEnemyAttackComponent::StopAttack()
+{
+	if (!IsValid(AnimInstance) || !IsValid(AttackMontage) || !AnimInstance->Montage_IsPlaying(AttackMontage))
+	{
+		return false;
+	}
+
+	AnimInstance->Montage_Stop(AttackMontage->GetDefaultBlendOutTime(), AttackMontage);
+	return true;
+}
+
+void UEnemyAttackComponent::InitiateAttack()
+{
+	if (!ProjectileClass)
+	{
+		return;
+	}
+
+	const FVector StartPoint = GetOwner()->GetActorLocation();
+	const float Theta = FMath::DegreesToRadians(AttackAngle / ProjectilesPerAttack);
+	const float Phi = FMath::DegreesToRadians(AttackAngle * 0.5) - Theta * 0.5;
+
+	for (int32 i = 0; i < ProjectilesPerAttack; i++)
+	{
+		FVector Direction = GetOwner()->GetActorForwardVector();
+		Direction = Direction.RotateAngleAxisRad(Theta * i - Phi, FVector::UpVector);
+		const FTransform SpawnTransform(FRotator::ZeroRotator, StartPoint + Direction * AttackOffset);
+		AEnemyProjectile* Projectile = GetWorld()->SpawnActorDeferred<AEnemyProjectile>(ProjectileClass, SpawnTransform);
+
+		if (Projectile)
+		{
+			Projectile->SetProjectileData(Direction, Damage);
+			Projectile->SetOwner(GetOwner());
+			Projectile->FinishSpawning(SpawnTransform);
+		}
+	}
+	
+	OnAttackInitiated.Broadcast(this);
+}
---
+++ Source/LudumDare56/Components/EnemyAttackComponent.h
@@ -0,0 +1,59 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "EnemyAttackComponent.generated.h"
+
+class AEnemyProjectile;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnAttackInitiatedDynamicSignature,
+                                            UEnemyAttackComponent*, Component);
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class LUDUMDARE56_API UEnemyAttackComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UEnemyAttackComponent();
+
+protected:
+	virtual void InitializeComponent() override;
+
+public:
+	UPROPERTY(BlueprintAssignable)
+	FOnAttackInitiatedDynamicSignature OnAttackInitiated;
+	
+	UPROPERTY(EditDefaultsOnly)
+	UAnimMontage* AttackMontage = nullptr;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	int32 Damage = 1;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TSubclassOf<AEnemyProjectile> ProjectileClass = nullptr;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	int32 ProjectilesPerAttack = 1;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	float AttackAngle = 0;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	FVector AttackOffset = FVector::Zero();
+
+	UFUNCTION(BlueprintCallable)
+	bool StartAttack();
+
+	UFUNCTION(BlueprintCallable)
+	bool StopAttack();
+
+	UFUNCTION(BlueprintCallable)
+	void InitiateAttack();
+
+protected:
+	UPROPERTY()
+	UAnimInstance* AnimInstance = nullptr;
+};
