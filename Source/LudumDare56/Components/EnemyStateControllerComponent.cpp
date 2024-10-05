1a0e8ce8478874cb83b9e534caeb492e6f8d12a6
Artyom Volkov <tricky.fat.cat@gmail.com>
2024-10-05 17:55:47 +0300

    Create enemy state controller component

---
+++ Source/LudumDare56/Components/EnemyStateControllerComponent.cpp
@@ -0,0 +1,100 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EnemyStateControllerComponent.h"
+
+
+UEnemyStateControllerComponent::UEnemyStateControllerComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+void UEnemyStateControllerComponent::InitializeComponent()
+{
+	Super::InitializeComponent();
+
+	if (GetWorld()->IsGameWorld())
+	{
+		EnterIdleState();
+	}
+}
+
+bool UEnemyStateControllerComponent::EnterIdleState()
+{
+	if (CurrentState == EEnemyState::Idle)
+	{
+		return false;
+	}
+
+	return ChangeState(EEnemyState::Idle);
+}
+
+bool UEnemyStateControllerComponent::EnterChaseState()
+{
+	if (CurrentState == EEnemyState::Death)
+	{
+		return false;
+	}
+
+	return ChangeState(EEnemyState::Chase);
+}
+
+bool UEnemyStateControllerComponent::EnterAttackState()
+{
+	if (CurrentState == EEnemyState::Death)
+	{
+		return false;
+	}
+
+	return ChangeState(EEnemyState::Attack);
+}
+
+bool UEnemyStateControllerComponent::EnterStunState(const float Duration)
+{
+	if (Duration <= 0.f || CurrentState == EEnemyState::Death)
+	{
+		return false;
+	}
+
+	if (ChangeState(EEnemyState::Stun))
+	{
+		GetWorld()->GetTimerManager().SetTimer(StunTimerHandle,
+		                                       Duration,
+		                                       &UEnemyStateControllerComponent::ExitStunState);
+	}
+
+	return false;
+}
+
+bool UEnemyStateControllerComponent::EnterDeathState()
+{
+	if (ChangeState(EEnemyState::Death))
+	{
+		StopStunTimer();
+		return true;
+	}
+
+	return false;
+}
+
+bool UEnemyStateControllerComponent::ChangeState(const EEnemyState NewState)
+{
+	if (NewState == CurrentState)
+	{
+		return false;
+	}
+
+	CurrentState = NewState;
+	OnStateChanged.Broadcast(this, CurrentState);
+	return true;
+}
+
+void UEnemyStateControllerComponent::ExitStunState()
+{
+	EnterIdleState();
+}
+
+void UEnemyStateControllerComponent::StopStunTimer()
+{
+	GetWorld()->GetTimerManager().ClearTimer(StunTimerHandle);
+}
---
+++ Source/LudumDare56/Components/EnemyStateControllerComponent.h
@@ -0,0 +1,68 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TrickyGameModeBase.h"
+#include "Components/ActorComponent.h"
+#include "EnemyStateControllerComponent.generated.h"
+
+UENUM(Blueprintable)
+enum class EEnemyState : uint8
+{
+	Idle,
+	Chase,
+	Attack,
+	Stun,
+	Death
+};
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnEnemyStateChangedDynamicSignature,
+                                             UEnemyStateControllerComponent*, Component,
+                                             EEnemyState, NewState);
+
+UCLASS(ClassGroup=(Enemy), meta=(BlueprintSpawnableComponent))
+class LUDUMDARE56_API UEnemyStateControllerComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UEnemyStateControllerComponent();
+
+protected:
+	virtual void InitializeComponent() override;
+
+public:
+	UPROPERTY(BlueprintAssignable)
+	FOnEnemyStateChangedDynamicSignature OnStateChanged;
+	
+	UFUNCTION(BlueprintCallable)
+	bool EnterIdleState();
+	
+	UFUNCTION(BlueprintCallable)
+	bool EnterChaseState();
+
+	UFUNCTION(BlueprintCallable)
+	bool EnterAttackState();
+
+	UFUNCTION(BlueprintCallable)
+	bool EnterStunState(const float Duration);
+
+	UFUNCTION(BlueprintCallable)
+	bool EnterDeathState();
+
+protected:
+	UPROPERTY(VisibleInstanceOnly)
+	EEnemyState CurrentState = EEnemyState::Stun;
+	
+	UFUNCTION()
+	bool ChangeState(const EEnemyState NewState);
+
+	FTimerHandle StunTimerHandle;
+
+	UFUNCTION()
+	void ExitStunState();
+
+	UFUNCTION()
+	void StopStunTimer();
+};
