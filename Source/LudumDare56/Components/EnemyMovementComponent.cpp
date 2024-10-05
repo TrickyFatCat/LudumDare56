31a7f13ff6f21a78539c3a40af21a97cfdf85c20
Artyom Volkov <tricky.fat.cat@gmail.com>
2024-10-05 18:11:27 +0300

    Create enemy movement component

---
+++ Source/LudumDare56/Components/EnemyMovementComponent.cpp
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EnemyMovementComponent.h"
+
+
+UEnemyMovementComponent::UEnemyMovementComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+	bAutoActivate = false;
+	bWantsInitializeComponent = true;
+}
+
+void UEnemyMovementComponent::InitializeComponent()
+{
+	Super::InitializeComponent();
+}
+
+void UEnemyMovementComponent::TickComponent(float DeltaTime,
+                                            ELevelTick TickType,
+                                            FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	
+	GetOwner()->AddActorWorldOffset(GetOwner()->GetActorForwardVector() * Speed * DeltaTime);
+}
---
+++ Source/LudumDare56/Components/EnemyMovementComponent.h
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "EnemyMovementComponent.generated.h"
+
+
+UCLASS(ClassGroup=(Enemy), meta=(BlueprintSpawnableComponent))
+class LUDUMDARE56_API UEnemyMovementComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UEnemyMovementComponent();
+
+protected:
+	virtual void InitializeComponent() override;
+	
+public:
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
+	
+public:
+	UFUNCTION(BlueprintGetter)
+	float GetSpeed() const { return Speed;}
+
+protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintGetter=GetSpeed)
+	float Speed = 500.f;
+};
