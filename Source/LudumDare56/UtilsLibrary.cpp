35c48c55e4951fd70bd4fbc21d9c7529e817f18e
Artyom Volkov <tricky.fat.cat@gmail.com>
2024-10-05 13:37:48 +0300

    Create utility library

---
+++ Source/LudumDare56/UtilsLibrary.cpp
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UtilsLibrary.h"
+
+#include "Components/HitPointsComponent.h"
+
+bool UUtilsLibrary::ApplyHeal(AActor* TargetActor, const int32 HealPower)
+{
+	if (!IsValid(TargetActor))
+	{
+		return false;
+	}
+
+	UHitPointsComponent* HitPointsComponent = TargetActor->GetComponentByClass<UHitPointsComponent>();
+
+	if (!IsValid(HitPointsComponent))
+	{
+		return false;
+	}
+
+	return HitPointsComponent->IncreaseHitPoints(HealPower);
+}
---
+++ Source/LudumDare56/UtilsLibrary.h
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "UtilsLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LUDUMDARE56_API UUtilsLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable)
+	bool ApplyHeal(AActor* TargetActor, const int32 HealPower);
+};
