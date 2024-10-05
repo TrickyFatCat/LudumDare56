fb2133f358ca6aeb841f99a79c2f1877b2f03814
Artyom Volkov <tricky.fat.cat@gmail.com>
2024-10-05 12:52:18 +0300

    Create hit points component

---
+++ Source/LudumDare56/Components/HitPointsComponent.cpp
@@ -0,0 +1,71 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HitPointsComponent.h"
+
+
+UHitPointsComponent::UHitPointsComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+	bWantsInitializeComponent = true;
+}
+
+void UHitPointsComponent::InitializeComponent()
+{
+	Super::InitializeComponent();
+
+	HitPoints = MaxHitPoints;
+}
+
+bool UHitPointsComponent::IncreaseHitPoints(const int32 Amount)
+{
+	if (Amount <= 0 || HitPoints <= 0 || HitPoints >= MaxHitPoints)
+	{
+		return false;
+	}
+
+	HitPoints += Amount;
+	HitPoints = FMath::Min(HitPoints, MaxHitPoints);
+	OnHitPointsIncreased.Broadcast(this, HitPoints, Amount, GetNormalizedHitPoints());
+	return true;
+}
+
+bool UHitPointsComponent::DecreaseHitPoints(const int32 Amount)
+{
+	if (Amount <= 0 || HitPoints <= 0)
+	{
+		return false;
+	}
+
+	HitPoints -= Amount;
+	HitPoints = FMath::Max(HitPoints, 0);
+	OnHitPointsDecreased.Broadcast(this, HitPoints, Amount, GetNormalizedHitPoints());
+
+	if (HitPoints <= 0)
+	{
+		OnZeroHitPoints.Broadcast(this);
+	}
+
+	return true;
+}
+
+void UHitPointsComponent::SetMaxHitPoints(const int32 Value)
+{
+	if (Value <= 0)
+	{
+		return;
+	}
+
+	MaxHitPoints = Value;
+	HitPoints = MaxHitPoints;
+}
+
+float UHitPointsComponent::GetNormalizedHitPoints() const
+{
+	if (MaxHitPoints <= 0)
+	{
+		return -1.f;
+	}
+
+	return static_cast<float>(HitPoints) / static_cast<float>(MaxHitPoints);
+}
---
+++ Source/LudumDare56/Components/HitPointsComponent.h
@@ -0,0 +1,72 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "HitPointsComponent.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_FourParams(FOnHitPointsIncreasedDynamicSignature,
+                                              UHitPointsComponent*, Component,
+                                              int32, NewHitPoints,
+                                              int32, DeltaHitPoints,
+                                              float, NormalizedHitPoints);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_FourParams(FOnHitPointsDecreasedDynamicSignature,
+                                              UHitPointsComponent*, Component,
+                                              int32, NewHitPoints,
+                                              int32, DeltaHitPoints,
+                                              float, NormalizedHitPoints);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnZeroHitPointsDynamicSignature,
+                                            UHitPointsComponent*, Component);
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class LUDUMDARE56_API UHitPointsComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UHitPointsComponent();
+
+protected:
+	virtual void InitializeComponent() override;
+
+public:
+	UPROPERTY(BlueprintAssignable)
+	FOnHitPointsIncreasedDynamicSignature OnHitPointsIncreased;
+	
+	UPROPERTY(BlueprintAssignable)
+	FOnHitPointsDecreasedDynamicSignature OnHitPointsDecreased;
+
+	UPROPERTY(BlueprintAssignable)
+	FOnZeroHitPointsDynamicSignature OnZeroHitPoints;
+	
+	UFUNCTION(BlueprintCallable)
+	bool IncreaseHitPoints(const int32 Amount);
+
+	UFUNCTION(BlueprintCallable)
+	bool DecreaseHitPoints(const int32 Amount);
+
+	UFUNCTION(BlueprintGetter)
+	int32 GetHitPoints() const { return HitPoints; }
+
+	UFUNCTION(BlueprintGetter)
+	int32 GetMaxHitPoints() const { return MaxHitPoints; }
+
+	UFUNCTION(BlueprintSetter)
+	void SetMaxHitPoints(const int32 Value);
+
+	UFUNCTION(BlueprintPure)
+	float GetNormalizedHitPoints() const;
+
+private:
+	UPROPERTY(VisibleInstanceOnly, BlueprintGetter=GetHitPoints)
+	int32 HitPoints = 1;
+
+	UPROPERTY(EditDefaultsOnly,
+		BlueprintGetter=GetMaxHitPoints,
+		BlueprintSetter=SetMaxHitPoints,
+		meta=(AllowPrivateAccess=true))
+	int32 MaxHitPoints = 1;
+};
