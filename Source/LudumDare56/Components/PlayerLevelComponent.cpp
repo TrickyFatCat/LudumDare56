2ddb73ce6c903e7069099c18934ff964223a734c
Artyom Volkov <tricky.fat.cat@gmail.com>
2024-10-05 14:04:56 +0300

    Create player level component

---
+++ Source/LudumDare56/Components/PlayerLevelComponent.cpp
@@ -0,0 +1,63 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayerLevelComponent.h"
+
+
+UPlayerLevelComponent::UPlayerLevelComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+	bWantsInitializeComponent = true;
+}
+
+void UPlayerLevelComponent::InitializeComponent()
+{
+	Super::InitializeComponent();
+	CalculateRequiredExperience();
+}
+
+bool UPlayerLevelComponent::IncreaseExperience(const int32 Amount)
+{
+	if (Amount <= 0)
+	{
+		return false;
+	}
+
+	CurrentExperience += Amount;
+
+	if (CurrentExperience >= RequiredExperience)
+	{
+		IncreaseLevel();
+		CurrentExperience = CurrentExperience - RequiredExperience;
+		CalculateRequiredExperience();
+	}
+
+	OnCurrentExperienceIncreased.Broadcast(this, CurrentExperience, GetNormalizedExperience());
+	return true;
+}
+
+float UPlayerLevelComponent::GetNormalizedExperience() const
+{
+	if (RequiredExperience <= 0)
+	{
+		return -1.f;
+	}
+
+	return static_cast<float>(CurrentExperience) / static_cast<float>(RequiredExperience);
+}
+
+void UPlayerLevelComponent::IncreaseLevel()
+{
+	CurrentLevel += 1;
+	OnLevelIncreased.Broadcast(this, CurrentLevel);
+}
+
+void UPlayerLevelComponent::CalculateRequiredExperience()
+{
+	if (!IsValid(ExperienceCurve))
+	{
+		return;
+	}
+
+	RequiredExperience = static_cast<int32>(ExperienceCurve->GetFloatValue(CurrentLevel));
+}
---
+++ Source/LudumDare56/Components/PlayerLevelComponent.h
@@ -0,0 +1,69 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "PlayerLevelComponent.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnLevelIncreasedDyanmicSignature,
+                                             UPlayerLevelComponent*, Component,
+                                             const int32, NewLevel);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_ThreeParams(FOnCurrentExperienceIncreasedDynamicSignature,
+                                               UPlayerLevelComponent*, Component,
+                                               const int32, CurrentExperience,
+                                               const float, NormalizedExperience);
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class LUDUMDARE56_API UPlayerLevelComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UPlayerLevelComponent();
+
+protected:
+	virtual void InitializeComponent() override;
+
+public:
+	UPROPERTY(BlueprintAssignable)
+	FOnLevelIncreasedDyanmicSignature OnLevelIncreased;
+	
+	UPROPERTY(BlueprintAssignable)
+	FOnCurrentExperienceIncreasedDynamicSignature OnCurrentExperienceIncreased;
+
+	UPROPERTY(EditDefaultsOnly)
+	UCurveFloat* ExperienceCurve = nullptr;
+	
+	UFUNCTION(BlueprintCallable)
+	bool IncreaseExperience(const int32 Amount);
+
+	UFUNCTION(BlueprintGetter)
+	int32 GetCurrentLevel() const { return CurrentLevel; }
+
+	UFUNCTION(BlueprintGetter)
+	int32 GetCurrentExperience() const { return CurrentExperience; }
+
+	UFUNCTION(BlueprintGetter)
+	int32 GetRequiredExperience() const { return RequiredExperience; }
+
+	UFUNCTION(BlueprintPure)
+	float GetNormalizedExperience() const;
+	
+private:
+	UPROPERTY(VisibleInstanceOnly, BlueprintGetter=GetCurrentLevel)
+	int32 CurrentLevel = 0;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintGetter=GetCurrentExperience)
+	int32 CurrentExperience = 0;
+
+	UPROPERTY(VisibleInstanceOnly, BulueprintGetter=GetRequiredExperience)
+	int32 RequiredExperience = 100;
+
+	UFUNCTION()
+	void IncreaseLevel();
+
+	UFUNCTION()
+	void CalculateRequiredExperience();
+};
