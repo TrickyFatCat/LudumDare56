5cc9e313d06c90dbc2764bb4c76c4867a298bc09
Artyom Volkov <tricky.fat.cat@gmail.com>
2024-10-05 19:32:28 +0300

    Create anim notify to initiate attack

---
+++ Source/LudumDare56/Animations/AnimNotify_InitiateAttack.cpp
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AnimNotify_InitiateAttack.h"
+
+#include "LudumDare56/Components/EnemyAttackComponent.h"
+
+void UAnimNotify_InitiateAttack::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation,
+                                        const FAnimNotifyEventReference& EventReference)
+{
+	Super::Notify(MeshComp, Animation, EventReference);
+	
+	UEnemyAttackComponent* AttackComponent = MeshComp->GetOwner()->GetComponentByClass<UEnemyAttackComponent>();
+
+	if (AttackComponent)
+	{
+		AttackComponent->InitiateAttack();
+	}
+}
---
+++ Source/LudumDare56/Animations/AnimNotify_InitiateAttack.h
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotify.h"
+#include "AnimNotify_InitiateAttack.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LUDUMDARE56_API UAnimNotify_InitiateAttack : public UAnimNotify
+{
+	GENERATED_BODY()
+
+	virtual void Notify(USkeletalMeshComponent* MeshComp,
+	                    UAnimSequenceBase* Animation,
+	                    const FAnimNotifyEventReference& EventReference) override;
+};
