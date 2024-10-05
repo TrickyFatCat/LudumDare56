647d2f877c30be8c36187a52170bbc93d31d33b4
Artyom Volkov <tricky.fat.cat@gmail.com>
2024-10-05 19:41:40 +0300

    Create enemy death component

---
+++ Source/LudumDare56/Components/EnemyDeathComponent.cpp
@@ -0,0 +1,38 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EnemyDeathComponent.h"
+
+
+UEnemyDeathComponent::UEnemyDeathComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+	bWantsInitializeComponent = true;
+}
+
+void UEnemyDeathComponent::InitializeComponent()
+{
+	Super::InitializeComponent();
+
+
+	if (GetWorld()->IsGameWorld())
+	{
+		USkeletalMeshComponent* SkeletalMeshComponent = GetOwner()->GetComponentByClass<USkeletalMeshComponent>();
+
+		if (IsValid(SkeletalMeshComponent))
+		{
+			AnimInstance = SkeletalMeshComponent->GetAnimInstance();
+		}
+	}
+}
+
+bool UEnemyDeathComponent::StartDeathSequence()
+{
+	if (!IsValid(AnimInstance) && !IsValid(DeathMontage))
+	{
+		return false;
+	}
+
+	AnimInstance->Montage_Play(DeathMontage);
+	return true;
+}
---
+++ Source/LudumDare56/Components/EnemyDeathComponent.h
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "EnemyDeathComponent.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class LUDUMDARE56_API UEnemyDeathComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UEnemyDeathComponent();
+
+protected:
+	virtual void InitializeComponent() override;
+
+public:
+	UPROPERTY(EditDefaultsOnly)
+	UAnimMontage* DeathMontage = nullptr;
+
+	UFUNCTION(BlueprintCallable)
+	bool StartDeathSequence();
+
+protected:
+	UPROPERTY()
+	UAnimInstance* AnimInstance = nullptr;
+};
