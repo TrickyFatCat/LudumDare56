b4b71da50715cde2a00c9fe39c7691bc8761c80d
Artyom Volkov <tricky.fat.cat@gmail.com>
2024-10-05 13:03:04 +0300

    Create player tower class

---
+++ Source/LudumDare56/Player/PlayerTower.cpp
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayerTower.h"
+
+#include "LudumDare56/Components/HitPointsComponent.h"
+
+
+APlayerTower::APlayerTower()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	HitPointsComponent = CreateDefaultSubobject<UHitPointsComponent>(TEXT("HitPoints"));
+}
+
+void APlayerTower::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void APlayerTower::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void APlayerTower::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
---
+++ Source/LudumDare56/Player/PlayerTower.h
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "PlayerTower.generated.h"
+
+class UHitPointsComponent;
+
+UCLASS()
+class LUDUMDARE56_API APlayerTower : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	APlayerTower();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Components)
+	TObjectPtr<UHitPointsComponent> HitPointsComponent = nullptr;
+};
