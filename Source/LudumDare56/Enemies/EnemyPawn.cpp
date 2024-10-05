a141fe30aa464a9c18a84fd5b5d56e21b41003a6
Artyom Volkov <tricky.fat.cat@gmail.com>
2024-10-05 18:17:07 +0300

    Create EnemyPawn class

---
+++ Source/LudumDare56/Enemies/EnemyPawn.cpp
@@ -0,0 +1,62 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EnemyPawn.h"
+
+#include "MovieSceneSequenceID.h"
+#include "Components/CapsuleComponent.h"
+#include "LudumDare56/Components/EnemyMovementComponent.h"
+#include "LudumDare56/Components/EnemyStateControllerComponent.h"
+#include "LudumDare56/Components/HitPointsComponent.h"
+
+
+AEnemyPawn::AEnemyPawn()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	Root = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	SetRootComponent(Root);
+
+	CapsuleComponent = CreateDefaultSubobject<UCapsuleComponent>(TEXT("CapsuleComponent"));
+	CapsuleComponent->SetupAttachment(GetRootComponent());
+
+	SkeletalMeshComponent = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("SkeletalMeshComponent"));
+	SkeletalMeshComponent->SetupAttachment(GetRootComponent());
+
+	HitPointsComponent = CreateDefaultSubobject<UHitPointsComponent>(TEXT("HitPointsComponent"));
+	EnemyStateControllerComponent = CreateDefaultSubobject<
+		UEnemyStateControllerComponent>(TEXT("EnemyStateController"));
+	EnemyMovementComponent = CreateDefaultSubobject<UEnemyMovementComponent>(TEXT("EnemyMovement"));
+}
+
+void AEnemyPawn::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AEnemyPawn::HandleEnemyStateChanged(UEnemyStateControllerComponent* Component, EEnemyState NewState)
+{
+	switch (NewState)
+	{
+	case EEnemyState::Chase:
+		EnemyMovementComponent->Activate();
+		break;
+	default:
+		EnemyMovementComponent->Deactivate();
+		break;
+	}
+}
+
+void AEnemyPawn::HandleAnyDamageTaken(AActor* DamagedActor,
+                                      float Damage,
+                                      const UDamageType* DamageType,
+                                      AController* InstigatedBy,
+                                      AActor* DamageCauser)
+{
+	HitPointsComponent->DecreaseHitPoints(Damage);
+}
+
+void AEnemyPawn::HandleZeroHitPoints(UHitPointsComponent* Component)
+{
+	EnemyStateControllerComponent->EnterDeathState();
+}
---
+++ Source/LudumDare56/Enemies/EnemyPawn.h
@@ -0,0 +1,60 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "EnemyPawn.generated.h"
+
+enum class EEnemyState : uint8;
+class UEnemyMovementComponent;
+class UEnemyStateControllerComponent;
+class UCapsuleComponent;
+class UHitPointsComponent;
+
+UCLASS()
+class LUDUMDARE56_API AEnemyPawn : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	AEnemyPawn();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+protected:
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere)
+	TObjectPtr<USceneComponent> Root = nullptr;
+
+	UPROPERTY(BlueprintReadOnly, VisibleInstanceOnly)
+	TObjectPtr<UCapsuleComponent> CapsuleComponent = nullptr;
+
+	UPROPERTY(BlueprintReadOnly, VisibleInstanceOnly)
+	TObjectPtr<USkeletalMeshComponent> SkeletalMeshComponent = nullptr;
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere)
+	TObjectPtr<UHitPointsComponent> HitPointsComponent = nullptr;
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere)
+	TObjectPtr<UEnemyStateControllerComponent> EnemyStateControllerComponent = nullptr;
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere)
+	TObjectPtr<UEnemyMovementComponent> EnemyMovementComponent = nullptr;
+
+private:
+	UFUNCTION()
+	void HandleEnemyStateChanged(UEnemyStateControllerComponent* Component, EEnemyState NewState);
+
+	UFUNCTION()
+	void HandleAnyDamageTaken(AActor* DamagedActor,
+	                          float Damage,
+	                          const UDamageType* DamageType,
+	                          AController* InstigatedBy,
+	                          AActor* DamageCauser);
+
+
+	UFUNCTION()
+	void HandleZeroHitPoints(UHitPointsComponent* Component);
+};
