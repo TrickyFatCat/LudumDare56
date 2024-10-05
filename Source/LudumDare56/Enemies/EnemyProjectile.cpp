49e1269a47cc8f728f47a3fdb812fafe69793546
Artyom Volkov <tricky.fat.cat@gmail.com>
2024-10-05 18:43:03 +0300

    Create enemy projectile class

---
+++ Source/LudumDare56/Enemies/EnemyProjectile.cpp
@@ -0,0 +1,54 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EnemyProjectile.h"
+
+#include "Components/SphereComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+#include "Kismet/GameplayStatics.h"
+
+AEnemyProjectile::AEnemyProjectile()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	SphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("Sphere"));
+	SetRootComponent(SphereComponent);
+
+	ProjectileMovementComponent = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("ProjectileMovement"));
+}
+
+void AEnemyProjectile::BeginPlay()
+{
+	ProjectileMovementComponent->Velocity = MovementDirection * ProjectileMovementComponent->InitialSpeed;
+
+	SphereComponent->IgnoreActorWhenMoving(GetOwner(), true);
+	SphereComponent->OnComponentHit.AddDynamic(this, &AEnemyProjectile::HandleProjectileHit);
+
+	Super::BeginPlay();
+}
+
+void AEnemyProjectile::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+auto AEnemyProjectile::SetProjectileData(const FVector& Direction, const int32 NewDamage) -> void
+{
+	MovementDirection = Direction;
+	Damage = NewDamage <= 0 ? 1 : NewDamage;
+}
+
+void AEnemyProjectile::HandleProjectileHit(UPrimitiveComponent* HitComponent,
+                                           AActor* OtherActor,
+                                           UPrimitiveComponent* OtherComp,
+                                           FVector NormalImpulse,
+                                           const FHitResult& Hit)
+{
+	if (OtherActor)
+	{
+		UGameplayStatics::ApplyDamage(OtherActor, Damage, nullptr, this, nullptr);
+	}
+
+	OnHit.Broadcast();
+	Destroy();
+}
---
+++ Source/LudumDare56/Enemies/EnemyProjectile.h
@@ -0,0 +1,52 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "EnemyProjectile.generated.h"
+
+class USphereComponent;
+class UProjectileMovementComponent;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnHitDynamicSignature)
+
+UCLASS()
+class LUDUMDARE56_API AEnemyProjectile : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AEnemyProjectile();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly)
+	TObjectPtr<UProjectileMovementComponent> ProjectileMovementComponent;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly)
+	TObjectPtr<USphereComponent> SphereComponent;
+
+	UPROPERTY(BlueprintAssignable)
+	FOnHitDynamicSignature OnHit;
+
+public:
+	auto SetProjectileData(const FVector& Direction, const int32 NewDamage) -> void;
+
+private:
+	FVector MovementDirection = FVector::ForwardVector;
+
+	int32 Damage = 1;
+
+	UFUNCTION()
+	void HandleProjectileHit(UPrimitiveComponent* HitComponent,
+	                         AActor* OtherActor,
+	                         UPrimitiveComponent* OtherComp,
+	                         FVector NormalImpulse,
+	                         const FHitResult& Hit);
+};
