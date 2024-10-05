b227c0f5fa5f7b8a18545647eb14dbdf2e637167
Artyom Volkov <tricky.fat.cat@gmail.com>
2024-10-05 19:26:48 +0300

    Create enemy experience component

---
+++ Source/LudumDare56/Components/EnemyExperienceComponent.cpp
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EnemyExperienceComponent.h"
+
+#include "LudumDare56/UtilsLibrary.h"
+
+
+UEnemyExperienceComponent::UEnemyExperienceComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+void UEnemyExperienceComponent::InitializeComponent()
+{
+	Super::InitializeComponent();
+}
+
+bool UEnemyExperienceComponent::GiveExperience()
+{
+	return UUtilsLibrary::IncreasePlayerExperience(this, Experience);
+}
+
+void UEnemyExperienceComponent::SetExperience(const int32 Value)
+{
+	if (Value <= 0)
+	{
+		return;
+	}
+
+	Experience = Value;
+}
---
+++ Source/LudumDare56/Components/EnemyExperienceComponent.h
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "EnemyExperienceComponent.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class LUDUMDARE56_API UEnemyExperienceComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UEnemyExperienceComponent();
+
+protected:
+	virtual void InitializeComponent() override;
+
+public:
+	UFUNCTION(BlueprintCallable)
+	bool GiveExperience();
+	
+	UFUNCTION(BlueprintGetter)
+	int32 GetExperience() const { return Experience; }
+
+	UFUNCTION(BlueprintSetter)
+	void SetExperience(const int32 Value);
+
+protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintGetter=GetExperience, BlueprintSetter=SetExperience)
+	int32 Experience = 10;
+};
