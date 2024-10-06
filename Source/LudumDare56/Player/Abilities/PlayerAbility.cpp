a454827718e881f561a372240eabc78106b5007a
Artyom Volkov <tricky.fat.cat@gmail.com>
2024-10-06 17:27:38 +0300

    Create player ability actor class

---
+++ Source/LudumDare56/Player/Abilities/PlayerAbility.cpp
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayerAbility.h"
+
+
+APlayerAbility::APlayerAbility()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	Root = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	SetRootComponent(Root);
+}
+
+void APlayerAbility::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void APlayerAbility::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ Source/LudumDare56/Player/Abilities/PlayerAbility.h
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "PlayerAbility.generated.h"
+
+UCLASS()
+class LUDUMDARE56_API APlayerAbility : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	APlayerAbility();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	TObjectPtr<USceneComponent> Root = nullptr;
+};
