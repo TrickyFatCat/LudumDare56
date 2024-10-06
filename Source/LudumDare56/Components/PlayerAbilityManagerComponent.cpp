97a456302ec76e5a2d2b9d278da9d1239325c283
Artyom Volkov <tricky.fat.cat@gmail.com>
2024-10-06 17:27:54 +0300

    Create player ability manager component

---
+++ Source/LudumDare56/Components/PlayerAbilityManagerComponent.cpp
@@ -0,0 +1,84 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayerAbilityManagerComponent.h"
+
+#include "LudumDare56/Player/Abilities/PlayerAbility.h"
+
+
+UPlayerAbilityManagerComponent::UPlayerAbilityManagerComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+	bWantsInitializeComponent = true;
+}
+
+void UPlayerAbilityManagerComponent::InitializeComponent()
+{
+	Super::InitializeComponent();
+
+	if (GetWorld()->IsGameWorld())
+	{
+		if (AbilitiesClasses.IsEmpty())
+		{
+			return;
+		}
+
+		FTransform SpawnTransform = FTransform::Identity;
+		Abilities.Empty();
+
+		for (auto AbilityClass : AbilitiesClasses)
+		{
+			APlayerAbility* NewAbility = GetWorld()->SpawnActorDeferred<APlayerAbility>(AbilityClass, SpawnTransform);
+			NewAbility->FinishSpawning(SpawnTransform);
+			NewAbility->AttachToActor(GetOwner(), FAttachmentTransformRules::SnapToTargetNotIncludingScale);
+			Abilities.Add(NewAbility);
+		}
+	}
+}
+
+void UPlayerAbilityManagerComponent::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+bool UPlayerAbilityManagerComponent::SelectAbility(TSubclassOf<APlayerAbility> Ability)
+{
+	if (!IsValid(Ability))
+	{
+		return false;
+	}
+
+	DeselectAbility(Ability);
+
+	if (!SelectedAbility->IsA(Ability))
+	{
+		auto Predicate = [&](const APlayerAbility* AbilityActor)
+		{
+			return AbilityActor->GetClass() == Ability;
+		};
+
+		SelectedAbility = *Abilities.FindByPredicate(Predicate);
+	}
+
+	return true;
+}
+
+bool UPlayerAbilityManagerComponent::DeselectAbility(TSubclassOf<APlayerAbility> Ability)
+{
+	if (!IsValid(Ability))
+	{
+		return false;
+	}
+
+	return true;
+}
+
+bool UPlayerAbilityManagerComponent::UseSelectedAbility()
+{
+	if (!IsValid(SelectedAbility))
+	{
+		return false;
+	}
+
+	return true;
+}
---
+++ Source/LudumDare56/Components/PlayerAbilityManagerComponent.h
@@ -0,0 +1,44 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "PlayerAbilityManagerComponent.generated.h"
+
+
+class APlayerAbility;
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class LUDUMDARE56_API UPlayerAbilityManagerComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UPlayerAbilityManagerComponent();
+
+protected:
+	virtual void InitializeComponent() override;
+
+	virtual void BeginPlay() override;
+
+public:
+	UFUNCTION(BlueprintCallable)
+	bool SelectAbility(TSubclassOf<APlayerAbility> Ability);
+
+	UFUNCTION(BlueprintCallable)
+	bool DeselectAbility(TSubclassOf<APlayerAbility> Ability);
+
+	UFUNCTION(BlueprintCallable)
+	bool UseSelectedAbility();
+
+protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+	TArray<TSubclassOf<APlayerAbility>> AbilitiesClasses;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly)
+	TArray<APlayerAbility*> Abilities;
+
+	UPROPERTY(VisibleInstanceOnly)
+	APlayerAbility* SelectedAbility = nullptr;
+};
