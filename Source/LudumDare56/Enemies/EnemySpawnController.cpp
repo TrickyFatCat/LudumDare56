6e1750542da30ab6e03a8409c22245e4e26f945d
Artyom Volkov <tricky.fat.cat@gmail.com>
2024-10-06 13:37:13 +0300

    Create enemy spawn controller class

---
+++ Source/LudumDare56/Enemies/EnemySpawnController.cpp
@@ -0,0 +1,154 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EnemySpawnController.h"
+
+#include "EnemyPawn.h"
+#include "Kismet/GameplayStatics.h"
+#include "LudumDare56/Components/HitPointsComponent.h"
+#include "LudumDare56/Components/PlayerLevelComponent.h"
+
+
+AEnemySpawnController::AEnemySpawnController()
+{
+	PrimaryActorTick.bCanEverTick = false;
+}
+
+void AEnemySpawnController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(this, 0);
+
+	if (IsValid(PlayerPawn))
+	{
+		UPlayerLevelComponent* PlayerLevelComponent = PlayerPawn->GetComponentByClass<UPlayerLevelComponent>();
+
+		if (IsValid(PlayerLevelComponent))
+		{
+			PlayerLevelComponent->OnLevelIncreased.AddUniqueDynamic(this,
+			                                                        &AEnemySpawnController::HandlePlayerLevelIncreased);
+		}
+	}
+	
+	SetSpawnData();
+	StartSpawn();
+}
+
+void AEnemySpawnController::StopSpawn()
+{
+	if (SpawnData.IsEmpty())
+	{
+		return;
+	}
+
+	for (auto Data : SpawnData)
+	{
+		GetWorldTimerManager().ClearTimer(Data.SpawnTimerHandle);
+	}
+}
+
+void AEnemySpawnController::ProcessEnemySpawn(TSubclassOf<AEnemyPawn> EnemyClass)
+{
+	auto Predicate = [&](FEnemySpawnData TargetData)
+	{
+		return TargetData.Enemy == EnemyClass;
+	};
+
+	FEnemySpawnData* Data = SpawnData.FindByPredicate(Predicate);
+
+	FTimerDelegate TimerDelegate;
+	TimerDelegate.BindUObject(this, &AEnemySpawnController::ProcessEnemySpawn, Data->Enemy);
+	GetWorldTimerManager().SetTimer(Data->SpawnTimerHandle,
+	                                TimerDelegate,
+	                                Data->SpawnDelay.GetValueAtLevel(CurrentLevel),
+	                                false);
+
+	const int32 MaxNum = Data->MaxNumber.GetValueAtLevel(CurrentLevel);
+
+	if (MaxNum <= 0 || Data->CurrentNumber >= MaxNum)
+	{
+		return;
+	}
+
+	if (!SpawnEnemy(Data->Enemy))
+	{
+		return;
+	}
+
+	Data->CurrentNumber++;
+}
+
+void AEnemySpawnController::SetSpawnData()
+{
+	if (!EnemySpawnDataTable)
+	{
+		return;
+	}
+
+	SpawnData.Empty();
+	TArray<FEnemySpawnData*> DataArray;
+	EnemySpawnDataTable->GetAllRows("", DataArray);
+
+	for (const auto Data : DataArray)
+	{
+		SpawnData.Add(*Data);
+	}
+}
+
+void AEnemySpawnController::StartSpawn()
+{
+	if (SpawnData.IsEmpty())
+	{
+		return;
+	}
+
+	for (auto Data : SpawnData)
+	{
+		if (!IsValid(Data.Enemy))
+		{
+			continue;
+		}
+
+		ProcessEnemySpawn(Data.Enemy);
+	}
+}
+
+bool AEnemySpawnController::SpawnEnemy(TSubclassOf<AEnemyPawn> EnemyClass)
+{
+	if (!IsValid(EnemyClass))
+	{
+		return false;
+	}
+
+	FTransform SpawnTransform = FTransform::Identity;
+	SpawnTransform.SetLocation(GetActorLocation());
+
+	AEnemyPawn* NewEnemy = GetWorld()->SpawnActorDeferred<AEnemyPawn>(EnemyClass, SpawnTransform);
+	UHitPointsComponent* HitPointsComponent = NewEnemy->GetComponentByClass<UHitPointsComponent>();
+
+	if (IsValid(HitPointsComponent))
+	{
+		HitPointsComponent->OnZeroHitPoints.AddUniqueDynamic(this, &AEnemySpawnController::HandleEnemyDeath);
+	}
+
+	NewEnemy->FinishSpawning(SpawnTransform);
+	return true;
+}
+
+void AEnemySpawnController::HandleEnemyDeath(UHitPointsComponent* Component)
+{
+	auto Predicate = [&](FEnemySpawnData TargetData)
+	{
+		return TargetData.Enemy == Component->GetOwner()->GetClass();
+	};
+
+	FEnemySpawnData& Data = *SpawnData.FindByPredicate(Predicate);
+	Data.CurrentNumber -= 1;
+	Component->OnZeroHitPoints.RemoveDynamic(this, &AEnemySpawnController::HandleEnemyDeath);
+}
+
+void AEnemySpawnController::HandlePlayerLevelIncreased(UPlayerLevelComponent* Component, int32 NewLevel)
+{
+	CurrentLevel = NewLevel;
+}
---
+++ Source/LudumDare56/Enemies/EnemySpawnController.h
@@ -0,0 +1,77 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ScalableFloat.h"
+#include "EnemySpawnController.generated.h"
+
+class UPlayerLevelComponent;
+class UHitPointsComponent;
+class AEnemyPawn;
+
+USTRUCT(BlueprintType)
+struct FEnemySpawnData : public FTableRowBase
+{
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TSubclassOf<AEnemyPawn> Enemy;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FScalableFloat SpawnDelay;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FScalableFloat MaxNumber;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	int32 CurrentNumber = 0;
+
+	UPROPERTY(BlueprintReadOnly)
+	FTimerHandle SpawnTimerHandle;
+};
+
+UCLASS()
+class LUDUMDARE56_API AEnemySpawnController : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AEnemySpawnController();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+	UDataTable* EnemySpawnDataTable = nullptr;
+
+	UFUNCTION(BlueprintCallable)
+	void StopSpawn();
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	TArray<FEnemySpawnData> SpawnData;
+
+	UPROPERTY()
+	int32 CurrentLevel = 0;
+
+	UFUNCTION()
+	void SetSpawnData();
+	
+	UFUNCTION()
+	void StartSpawn();
+
+	UFUNCTION()
+	void ProcessEnemySpawn(TSubclassOf<AEnemyPawn> EnemyClass);
+
+	UFUNCTION()
+	bool SpawnEnemy(TSubclassOf<AEnemyPawn> EnemyClass);
+
+	UFUNCTION()
+	void HandleEnemyDeath(UHitPointsComponent* Component);
+
+	UFUNCTION()
+	void HandlePlayerLevelIncreased(UPlayerLevelComponent* Component, int32 NewLevel);
+};
