de857f569bb08c97b3e2fd100b3e1b02aed63130
Artyom Volkov <tricky.fat.cat@gmail.com>
2024-10-06 16:26:41 +0300

    Create hit points regeneration component

---
+++ Source/LudumDare56/Components/HitPointsRegenerationComponent.h
@@ -0,0 +1,56 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "HitPointsRegenerationComponent.generated.h"
+
+
+class UHitPointsComponent;
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class LUDUMDARE56_API UHitPointsRegenerationComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UHitPointsRegenerationComponent();
+
+protected:
+	virtual void InitializeComponent() override;
+	
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+	float DefaultRegenerationRate = 10.f;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+	int32 DefaultRegenerationPower = 1;
+	
+protected:
+	UPROPERTY()
+	TObjectPtr<UHitPointsComponent> HitPointsComponent = nullptr;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly)
+	float RegenerationRate = DefaultRegenerationRate;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly)
+	int32 RegenerationPower = DefaultRegenerationPower;
+
+	UPROPERTY()
+	FTimerHandle RegenerationTimerHandle;
+
+	UFUNCTION()
+	void StartRegeneration();
+
+	UFUNCTION()
+	void RegenerateHitPoints();
+
+	UFUNCTION()
+	void StopRegeneration();
+
+	UFUNCTION()
+	void HandleZeroHitPoints();
+};
+
+
