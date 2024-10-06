0249f3623d0976f6e7ed1ebe06cda995ddb9bd86
Artyom Volkov <tricky.fat.cat@gmail.com>
2024-10-06 09:01:07 +0300

    Add dependencies

--- Source/LudumDare56/LudumDare56.Build.cs
+++ Source/LudumDare56/LudumDare56.Build.cs
@@ -8,7 +8,7 @@ public class LudumDare56 : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "TrickyGameMode" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "TrickyGameMode", "GameplayAbilities", "GameplayTags", "GameplayTasks" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
