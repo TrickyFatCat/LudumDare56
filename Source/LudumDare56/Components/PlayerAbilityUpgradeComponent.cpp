2ebe06438810137886b3de88aecc97c548fed536
Artyom Volkov <tricky.fat.cat@gmail.com>
2024-10-07 10:35:33 +0300

    Create player ability upgrade component

---
+++ Source/LudumDare56/Components/PlayerAbilityUpgradeComponent.cpp
@@ -0,0 +1,115 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayerAbilityUpgradeComponent.h"
+
+#include "Kismet/GameplayStatics.h"
+#include "LudumDare56/Player/Abilities/AbilityUpgrade.h"
+
+
+UPlayerAbilityUpgradeComponent::UPlayerAbilityUpgradeComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+	bWantsInitializeComponent = true;
+}
+
+void UPlayerAbilityUpgradeComponent::InitializeComponent()
+{
+	Super::InitializeComponent();
+
+	if (GetWorld()->IsGameWorld())
+	{
+		PopulateUpgradesData();
+		CreateUpgradesObjects();
+	}
+}
+
+bool UPlayerAbilityUpgradeComponent::GetAbilityUpgradeDataByClass(TSubclassOf<UAbilityUpgrade> UpgradeClass,
+                                                                  FAbilityUpgradeData& OutData) const
+{
+	if (!IsValid(UpgradeClass) || UpgradesData.IsEmpty())
+	{
+		return false;
+	}
+
+	auto Predicate = [&](const FAbilityUpgradeData& Data)
+	{
+		return Data.UpgradeClass == UpgradeClass;
+	};
+
+	const FAbilityUpgradeData* DataPointer = UpgradesData.FindByPredicate(Predicate);
+
+	if (!DataPointer)
+	{
+		return false;
+	}
+
+	OutData = *DataPointer;
+	return true;
+}
+
+bool UPlayerAbilityUpgradeComponent::GetAbilityUpgradeDataByObject(UAbilityUpgrade* UpgradeObject,
+                                                                   FAbilityUpgradeData& OutData) const
+{
+	if (!IsValid(UpgradeObject) || UpgradesData.IsEmpty())
+	{
+		return false;
+	}
+
+	auto Predicate = [&](const FAbilityUpgradeData& Data)
+	{
+		return Data.UpgradeObject == UpgradeObject;
+	};
+
+	const FAbilityUpgradeData* DataPointer = UpgradesData.FindByPredicate(Predicate);
+
+	if (!DataPointer)
+	{
+		return false;
+	}
+
+	OutData = *DataPointer;
+	return true;
+}
+
+void UPlayerAbilityUpgradeComponent::PopulateUpgradesData()
+{
+	if (!UpgradesDataTable)
+	{
+		return;
+	}
+	
+	UpgradesData.Empty();
+	TArray<FAbilityUpgradeData*> DataArray;
+	UpgradesDataTable->GetAllRows("", DataArray);
+
+	for (const auto Data : DataArray)
+	{
+		UpgradesData.Add(*Data);
+	}
+}
+
+void UPlayerAbilityUpgradeComponent::CreateUpgradesObjects()
+{
+	if (UpgradesData.IsEmpty())
+	{
+		return;
+	}
+
+	for (auto& Data : UpgradesData)
+	{
+		if (!IsValid(Data.UpgradeClass))
+		{
+			continue;
+		}
+
+		UObject* NewUpgradeObject = UGameplayStatics::SpawnObject(Data.UpgradeClass, GetOwner());
+
+		if (!IsValid(NewUpgradeObject))
+		{
+			continue;
+		}
+
+		Data.UpgradeObject = Cast<UAbilityUpgrade>(NewUpgradeObject);
+	}
+}
---
+++ Source/LudumDare56/Components/PlayerAbilityUpgradeComponent.h
@@ -0,0 +1,68 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "PlayerAbilityUpgradeComponent.generated.h"
+
+class UAbilityUpgrade;
+
+USTRUCT(BlueprintType)
+struct FAbilityUpgradeData : public FTableRowBase
+{
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TSubclassOf<UAbilityUpgrade> UpgradeClass = nullptr;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly)
+	UAbilityUpgrade* UpgradeObject = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FText Name = FText::FromString(TEXT("Ability Name"));
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FText Description = FText::FromString(TEXT("Ability Description"));
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UTexture2D* Icon = nullptr;
+};
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class LUDUMDARE56_API UPlayerAbilityUpgradeComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UPlayerAbilityUpgradeComponent();
+
+protected:
+	virtual void InitializeComponent() override;
+
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+	UDataTable* UpgradesDataTable = nullptr;
+
+	UFUNCTION(BlueprintGetter)
+	TArray<FAbilityUpgradeData> GetUpgradesData() const { return UpgradesData; }
+
+	UFUNCTION(BlueprintPure)
+	bool GetAbilityUpgradeDataByClass(TSubclassOf<UAbilityUpgrade> UpgradeClass, FAbilityUpgradeData& OutData) const;
+
+	UFUNCTION(BlueprintPure)
+	bool GetAbilityUpgradeDataByObject(UAbilityUpgrade* UpgradeObject, FAbilityUpgradeData& OutData) const;
+
+protected:
+	UPROPERTY(VisibleInstanceOnly, BlueprintGetter=GetUpgradesData)
+	TArray<FAbilityUpgradeData> UpgradesData;
+
+private:
+	UFUNCTION()
+	void PopulateUpgradesData();
+
+	UFUNCTION()
+	void CreateUpgradesObjects();
+};
+
+
