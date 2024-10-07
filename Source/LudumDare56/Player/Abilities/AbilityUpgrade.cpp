961883a7c8d07d8676bdc3d63268921d823685a2
Artyom Volkov <tricky.fat.cat@gmail.com>
2024-10-07 09:04:40 +0300

    Create ability upgrade class

---
+++ Source/LudumDare56/Player/Abilities/AbilityUpgrade.cpp
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AbilityUpgrade.h"
+
+#include "Kismet/GameplayStatics.h"
+#include "LudumDare56/Components/PlayerAbilityManagerComponent.h"
+
+void UAbilityUpgrade::ApplyUpgrade()
+{
+	ActivateUpgradeEffect();
+}
+
+void UAbilityUpgrade::InitUpgrade(const AActor* PlayerActor)
+{
+	if (!IsValid(PlayerActor))
+	{
+		return;
+	}
+
+	UPlayerAbilityManagerComponent* AbilityManagerComponent = PlayerActor->GetComponentByClass<UPlayerAbilityManagerComponent>();
+
+	if (!IsValid(AbilityManagerComponent))
+	{
+		return;
+	}
+
+	Ability = AbilityManagerComponent->GetAbilityByClass(AbilityClass);
+}
+
+void UAbilityUpgrade::ActivateUpgradeEffect_Implementation()
+{
+}
---
+++ Source/LudumDare56/Player/Abilities/AbilityUpgrade.h
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "AbilityUpgrade.generated.h"
+
+class APlayerAbility;
+/**
+ * 
+ */
+UCLASS()
+class LUDUMDARE56_API UAbilityUpgrade : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TSubclassOf<APlayerAbility> AbilityClass = nullptr;
+	
+	UFUNCTION()
+	void ApplyUpgrade();
+
+	UFUNCTION()
+	void InitUpgrade(const AActor* PlayerActor);
+
+protected:
+	UPROPERTY(BlueprintReadOnly)
+	APlayerAbility* Ability = nullptr;
+	
+	UFUNCTION(BlueprintNativeEvent)
+	void ActivateUpgradeEffect();
+};
